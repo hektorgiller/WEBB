020e2b5626254e5bbf3a5a566a8846c49f830bad
hektorgiller <33809158+hektorgiller@users.noreply.github.com>
2020-08-21 14:17:37 +0300

    Add files via upload

---
+++ DataTables/custom.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+    $("#tblDepartmanlar").DataTable({
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json"
+        }
+    });
+    $("#tblDepartmanlar").on("click", ".btnDepartmanSil", function () {
+        var btn = $(this);
+        bootbox.confirm("Departmanı silmek istediğinizden eöinmisiniz?", function (result) {
+            if (result) {
+                var id = btn.data("id");
+
+                $.ajax({
+                    type: "GET",
+                    URL: "Departman/Sil/" + id,
+                    success: function () {
+                        btn.parent().parent().remove();
+                    }
+
+
+                });
+            }
+
+        })
+
+
+
+    });
+});
+
+
+function CheckDateTypeIsValid(dateElement) {
+    var value = $(dateElement).val();
+    if (value == "") {
+        $.ajax(dateElement).valid("false");
+    }
+    else {
+        $.ajax(dateElement).valid();
+    }
+}
